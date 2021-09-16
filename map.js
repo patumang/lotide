ca04308c47dfd1464d843969d37b4b478b647337
Umang Patel <umangpatel1991.cmail.com>
2021-09-16 19:20:51 -0400

    create a function similar to map inbuilt function to modify array elements

---
+++ map.js
@@ -0,0 +1,56 @@
+// function to compare arrays
+const eqArrays = function(arr1, arr2) {
+  
+  //compare that length of both the arrays are same
+  if (arr1.length === arr2.length) {
+    //initialize index with 0 and assume both arrays are equal, so equal = true
+    let index = 0;
+    let equal = true;
+
+    //loop through length of an array elements
+    while (index < arr1.length) {
+      //if values of both array at current index is not same store false to equal and break out of loop
+      if (arr1[index] !== arr2[index]) {
+        equal = false;
+        break;
+      }
+      index++;
+    }
+    //return false or true
+    return equal;
+
+  } else {
+    //if array lengths of both array are not same then return false
+    return false;
+  }
+
+};
+
+//function to show assert arrays are equal or not
+const assertArraysEqual = function(actual, expected) {
+  //store emoji from decimal codes
+  const emojiPassed = String.fromCodePoint(128512);
+  const emojiFailed = String.fromCodePoint(128125);
+
+  //check if both arrays are equal or not and log appropriate result
+  const equalArrays = eqArrays(actual, expected);
+  if (equalArrays)
+    console.log(`${emojiPassed}${emojiPassed}${emojiPassed} Assertion Passed: [${actual}] === [${expected}]`);
+  else
+    console.log(`${emojiFailed}${emojiFailed}${emojiFailed} Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+const map = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"]);
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
+assertArraysEqual(map(words, word => word.toUpperCase()), ["GROUND","CONTROL","TO","MAJOR","TOM"]);
