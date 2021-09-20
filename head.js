e8c3d1c1a63b88b87c1c8cd4daad355ded3fe2ef
Umang Patel <umangpatel1991.cmail.com>
2021-09-20 01:20:25 -0400

    included assertEqual function and removed previously copied assertEqual for refactoring

--- head.js
+++ head.js
@@ -1,34 +1,17 @@
 // FUNCTION IMPLEMENTATION
 const head = function(inputArray) {
 
-  //check that inputArray is an array otherwise return undefined
-  if (typeof inputArray === "object") {
-    //check for inputArray is not empty otherwise return undefined
-    if (inputArray.length > 0)
-      return inputArray[0];
-    else
-      return undefined;
-  } else {
+  //check if inputArray is not an array then return undefined
+  if (!Array.isArray(inputArray)) {
     return undefined;
   }
+
+  //check for inputArray is empty then return undefined
+  if (inputArray.length <= 0) {
+    return undefined;
+  }
+
+  return inputArray[0];
 };
 
-const assertEqual = function(actual, expected) {
-  //store emoji from decimal codes
-  const emojiPassed = String.fromCodePoint(128512);
-  const emojiFailed = String.fromCodePoint(128549);
-
-  //check if both arguments are same or not and log appropriate result
-  if (actual === expected)
-    console.log(`${emojiPassed} Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`${emojiFailed} Assertion Failed: ${actual} !== ${expected}`);
-};
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 1);
-assertEqual(head(["5"]), "5");
-assertEqual(head("1"), "1");
-assertEqual(head(), false);
+module.exports = head;
