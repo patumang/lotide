cec2f641d9f5f40bd6b092a8da85165b311776ab
Umang Patel <umangpatel1991.cmail.com>
2021-09-20 01:54:04 -0400

    added modules exports statement to eqArrays function and removed copied assertEqual function

--- eqArrays.js
+++ eqArrays.js
@@ -1,46 +1,30 @@
 // function to compare arrays
 const eqArrays = function(arr1, arr2) {
   
-  //compare that length of both the arrays are same
-  if (arr1.length === arr2.length) {
-    //initialize index with 0 and assume both arrays are equal, so equal = true
-    let index = 0;
-    let equal = true;
+  //check if arr1 or arr2 is not an array then return undefined
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return undefined;
+  }
 
-    //loop through length of an array elements
-    while (index < arr1.length) {
-      //if values of both array at current index is not same store false to equal and break out of loop
-      if (arr1[index] !== arr2[index]) {
-        equal = false;
-        break;
-      }
-      index++;
-    }
-    //return false or true
-    return equal;
-
-  } else {
-    //if array lengths of both array are not same then return false
+  //compare if length of both the arrays are not same, return false
+  if (arr1.length !== arr2.length) {
     return false;
   }
 
+  //initialize index with 0 and assume both arrays are equal, so equal = true
+  let index = 0;
+
+  //loop through length of an array elements
+  while (index < arr1.length) {
+    //if values of both array at current index is not same,return false
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+    index++;
+  }
+  //if all elements are same, return true
+  return true;
+
 };
 
-//function to check actual and expected are same or not
-const assertEqual = function(actual, expected) {
-  //store emoji from decimal codes
-  const emojiPassed = String.fromCodePoint(128512);
-  const emojiFailed = String.fromCodePoint(128549);
-
-  //check if both arguments are same or not and log appropriate result
-  if (actual === expected)
-    console.log(`${emojiPassed} Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`${emojiFailed} Assertion Failed: ${actual} !== ${expected}`);
-};
-
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+module.exports = eqArrays;
