67c19e96b98f928e17b18b2983a8b68e5032e77a
Umang Patel <umangpatel1991.cmail.com>
2021-09-20 01:18:46 -0400

    added modules exports statement to export assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -1,22 +1,23 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
+
+  //check if actual or expected is an array or object show error message
+  if (typeof actual === "object" || typeof expected === "object") {
+    console.log("Invalid Input!");
+    return;
+  }
+
   //store emoji from decimal codes
   const emojiPassed = String.fromCodePoint(128512);
-  const emojiFailed = String.fromCodePoint(128549);
+  const emojiFailed = String.fromCodePoint(128125);
 
   //check if both arguments are same or not and log appropriate result
-  if (actual === expected)
-    console.log(`${emojiPassed} Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`${emojiFailed} Assertion Failed: ${actual} !== ${expected}`);
+  if (actual === expected) {
+    console.log(`${emojiPassed}${emojiPassed}${emojiPassed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emojiFailed}${emojiFailed}${emojiFailed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+    
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual(1, 5);
-assertEqual(1, "1");
-assertEqual(true, false);
-assertEqual(false);
-assertEqual();
+module.exports = assertEqual;
