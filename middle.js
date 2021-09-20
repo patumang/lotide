30f447702be528b8e38b817bad92422a0254eed6
Umang Patel <umangpatel1991.cmail.com>
2021-09-20 14:12:37 -0400

    added modules exports statement to middle function and removed copied eqArrays and assertArraysEqual functions

--- middle.js
+++ middle.js
@@ -1,45 +1,3 @@
-// function to compare arrays
-const eqArrays = function(arr1, arr2) {
-  
-  //compare that length of both the arrays are same
-  if (arr1.length === arr2.length) {
-    //initialize index with 0 and assume both arrays are equal, so equal = true
-    let index = 0;
-    let equal = true;
-
-    //loop through length of an array elements
-    while (index < arr1.length) {
-      //if values of both array at current index is not same store false to equal and break out of loop
-      if (arr1[index] !== arr2[index]) {
-        equal = false;
-        break;
-      }
-      index++;
-    }
-    //return false or true
-    return equal;
-
-  } else {
-    //if array lengths of both array are not same then return false
-    return false;
-  }
-
-};
-
-//function to show assert arrays are equal or not
-const assertArraysEqual = function(actual, expected) {
-  //store emoji from decimal codes
-  const emojiPassed = String.fromCodePoint(128512);
-  const emojiFailed = String.fromCodePoint(128549);
-
-  //check if both arrays are equal or not and log appropriate result
-  const equalArrays = eqArrays(actual, expected);
-  if (equalArrays)
-    console.log(`${emojiPassed} Assertion Passed: [${actual}] === [${expected}]`);
-  else
-    console.log(`${emojiFailed} Assertion Failed: [${actual}] !== [${expected}]`);
-};
-
 //function middle to find middle element of an array
 const middle = function(myArray) {
   //if the input argument is not an array, return function with error message
@@ -70,13 +28,4 @@ const middle = function(myArray) {
 
 };
 
-// TEST CODE
-const words = ["hello", "world", "lighthouse"];
-assertArraysEqual(middle(words), ["world"]);
-
-assertArraysEqual(middle("hello"), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
