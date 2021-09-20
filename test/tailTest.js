0978c6d7a8850e08ccb53be0324aa471d429a48f
Umang Patel <umangpatel1991.cmail.com>
2021-09-20 01:40:24 -0400

    added tail and assertEqual modules and test statements to test tail function

---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+// Test Case: Check the original array
+let words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+words = ["Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
