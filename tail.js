39e75a12e59f0525cf8dd64e9e59f08e7aa41e14
Umang Patel <umangpatel1991.cmail.com>
2021-09-20 01:38:50 -0400

    added modules exports statement to  tail function and removed copied assertEqual function

--- tail.js
+++ tail.js
@@ -1,35 +1,17 @@
 // FUNCTION IMPLEMENTATION
 const tail = function(inputArray) {
 
-  //check that inputArray is an array otherwise return undefined
-  if (typeof inputArray === "object") {
-    //check for length of inputArray is greater than 1 otherwise return empty array
-    if (inputArray.length > 1)
-      console.log(inputArray.slice(1));
-    else
-      return [];
-  } else {
+  //check if inputArray is not an array then return undefined
+  if (!Array.isArray(inputArray)) {
     return undefined;
   }
+  
+  //check for length of inputArray if array elements are 1 or less, return empty array
+  if (inputArray.length <= 1)
+    return [];
+    
+  console.log("tail:");
+  console.log(inputArray.slice(1));
 };
 
-const assertEqual = function(actual, expected) {
-  //store emoji from decimal codes
-  const emojiPassed = String.fromCodePoint(128512);
-  const emojiFailed = String.fromCodePoint(128549);
-
-  //check if both arguments are same or not and log appropriate result
-  if (actual === expected)
-    console.log(`${emojiPassed} Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`${emojiFailed} Assertion Failed: ${actual} !== ${expected}`);
-};
-
-// Test Case: Check the original array
-let words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-words = ["Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
